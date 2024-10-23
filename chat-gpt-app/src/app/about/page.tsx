ae85fe8375002073b66c74a2831c1070f02cc5e1
Annie Liao <5109918+liaowow@users.noreply.github.com>
2024-10-23 13:39:58 -0400

    Add About page and header with links in RootLayout

---
+++ chat-gpt-app/src/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function AboutRoute() {
+  return (
+    <main>
+      <h1 className="text-4xl font-bold">About Page</h1>
+      <div className="mt-5">This is about page.</div>
+    </main>
+  );
+}
